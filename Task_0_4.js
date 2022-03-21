8f35759c8c25204248765b2e1ea516163bc735b9
M-anza <magomaanza@gmail.com>
2022-03-21 13:56:51 +0200

    task 0.4

---
+++ Task_0_4.js
@@ -0,0 +1,9 @@
+function evenOrOdd(input){
+    let remainder = (input % 2)
+    if ( remainder == 0 ){
+        console.log("even") 
+    }else{
+        console.log("odd") 
+    }    
+}
+evenOrOdd(6)
