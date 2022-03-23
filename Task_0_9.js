7ff4c95ba31818d8b52ee42d5dce5acb0e61dd6d
M-anza <magomaanza@gmail.com>
2022-03-23 13:33:34 +0200

    more changes requested

--- Task_0_9.js
+++ Task_0_9.js
@@ -3,11 +3,11 @@ function printVowels(word) {
   const vowel = ["a", "e", "i", "o", "u"];
   let results = "";
   for (let i = 0; i < word.length; i++) {
-    if (vowel.includes(word[i])) {
+    if (vowel.includes(word[i]) && !results.includes(word[i])) {
       let arr = vowel.splice(vowel.indexOf(word[i], 1));
       results += word[i];
     }
   }
   console.log("Vowels: " + results.split("").join(", "));
 }
-printVowels("MuhulisEni");
+printVowels("umuziacademy");
